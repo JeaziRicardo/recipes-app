fdf714b4651fdffbc4229f6fcdd7747112388857
NiiVx <vinicios241@gmail.com>
2022-06-20 17:09:30 -0300

    regex e minSenha

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,20 +1,25 @@
 import React from 'react';
-
 function Login() {
+  const validation = ({ target }) => {
+    const regexEmail = '^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$';
+    const minSenha = 7;
+  };
+  
   return (
     <form>
       <section>
         <input
           data-testid="email-input"
           type="email"
+          value={}
           placeholder="Email"
-          onChange={ ({ target }) => (target.value) }
+          onChange={ ({ target }) => (validation(target)) }
         />
         <input
           data-testid="password-input"
           type="password"
           placeholder="Password"
-          onChange={ ({ target }) => (target.value) }
+          onChange={ ({ target }) => (validation(target)) }
         />
         <button
           onClick={ () => {} }
