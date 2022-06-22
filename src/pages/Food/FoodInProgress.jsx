4990abb764caf5ea1f6b737019d137d8a9dc1a70
Guilherme Axhcar <guilhermeaxhcar@gmail.com>
2022-06-22 19:44:11 -0300

    Adiciona páginas e rotas

---
+++ src/pages/Food/FoodInProgress.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function FoodsInProgress() {
+  return (
+    <div>
+      <h1>a</h1>
+    </div>
+  );
+}
+
+export default FoodsInProgress;
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Header from '../components/Header';
 
 function Profile() {
   return (
     <div>
-      <h1 data-testid="page-title">Profile</h1>
+      <Header />
     </div>
   );
 }
