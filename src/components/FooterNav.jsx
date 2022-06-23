bf5c1fb7de8f1df5d8afc14c39d688b37b152eec
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2022-06-23 15:11:43 -0300

    [19] - Cria os botões de navegação no FooterNav

--- src/components/FooterNav.jsx
+++ src/components/FooterNav.jsx
@@ -1,13 +1,33 @@
 import React from 'react';
+import drinkIcon from '../images/drinkIcon.svg';
+import exploreIcon from '../images/exploreIcon.svg';
+import mealIcon from '../images/mealIcon.svg';
 
 function FooterNav() {
   return (
     <footer>
-      <nav>
-        
+      <nav data-testid="footer">
+        <button
+          data-testid="drinks-bottom-btn"
+          type="button"
+        >
+          <img src={ drinkIcon } alt="drinkIcon" />
+        </button>
+        <button
+          data-testid="explore-bottom-btn"
+          type="button"
+        >
+          <img src={ exploreIcon } alt="exploreIcon" />
+        </button>
+        <button
+          data-testid="food-bottom-btn"
+          type="button"
+        >
+          <img src={ mealIcon } alt="mealIcon" />
+        </button>
       </nav>
     </footer>
   );
-};
+}
 
 export default FooterNav;
