1d79ec1d7c35157b9727438a6068981f412a15a8
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2022-06-23 15:17:03 -0300

    [21] - Exibe o FooterNav nas telas indicadas

--- src/pages/Explores/ExploreDrinksIngredients.jsx
+++ src/pages/Explores/ExploreDrinksIngredients.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import FooterNav from '../../components/FooterNav';
 import Header from '../../components/Header';
 
 function ExploreDrinksIngredients() {
   return (
     <div>
       <Header />
+      <FooterNav />
     </div>
   );
 }
