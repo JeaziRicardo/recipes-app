433f7459240bad40d1bf8f435553b0c089dd2a3a
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2022-06-23 15:40:32 -0300

    [22 à 24] - Muda a rota ao clicar nos botoões de FooterNav

--- src/components/FooterNav.jsx
+++ src/components/FooterNav.jsx
@@ -1,27 +1,32 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import drinkIcon from '../images/drinkIcon.svg';
 import exploreIcon from '../images/exploreIcon.svg';
 import mealIcon from '../images/mealIcon.svg';
 
 function FooterNav() {
+  const history = useHistory();
   return (
     <footer>
       <nav data-testid="footer">
         <button
           data-testid="drinks-bottom-btn"
           type="button"
+          onClick={ () => (history.push('/drinks')) }
         >
           <img src={ drinkIcon } alt="drinkIcon" />
         </button>
         <button
           data-testid="explore-bottom-btn"
           type="button"
+          onClick={ () => (history.push('/explore')) }
         >
           <img src={ exploreIcon } alt="exploreIcon" />
         </button>
         <button
           data-testid="food-bottom-btn"
           type="button"
+          onClick={ () => (history.push('/foods')) }
         >
           <img src={ mealIcon } alt="mealIcon" />
         </button>
