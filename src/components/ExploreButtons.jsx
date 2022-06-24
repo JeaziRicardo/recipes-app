89c29ce409c6840b72e0a1932de2f61a58a068fb
NiiVx <vinicios241@gmail.com>
2022-06-24 19:00:05 -0300

    Cria componente ExploreButtons para o requisito 71

---
+++ src/components/ExploreButtons.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+function ExploreButtons() {
+  return (
+    <div>
+      <button
+        type="button"
+        data-testid="explore-by-ingredient"
+      >
+        By Ingredient
+      </button>
+      <button
+        type="button"
+        data-testid="explore-by-nationality"
+      >
+        By Nationality
+      </button>
+      <button
+        type="button"
+        data-testid="explore-surprise"
+      >
+        Surprise me!
+      </button>
+    </div>
+  );
+}
+
+export default ExploreButtons;
