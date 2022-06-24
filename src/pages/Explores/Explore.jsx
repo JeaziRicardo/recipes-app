eaf1866815c590a01d050bbf1071ec63110f0814
elviraraquelbasso@gmail.com <elviraraquelbasso@gmail.com>
2022-06-24 13:57:52 -0500

    [ 69 ]-Redireciona para explorar comida ou bebida ao clicar nos botões da pagina explorar

--- src/pages/Explores/Explore.jsx
+++ src/pages/Explores/Explore.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import FooterNav from '../../components/FooterNav';
 import Header from '../../components/Header';
 
 function Explore() {
+  const history = useHistory();
+
   return (
     <div>
       <Header title="Explore" />
@@ -10,12 +13,14 @@ function Explore() {
         <button
           type="button"
           data-testid="explore-foods"
+          onClick={ () => history.push('/explore/foods') }
         >
           Explore Foods
         </button>
         <button
           type="button"
-          data-testid="explore-foods"
+          data-testid="explore-drinks"
+          onClick={ () => history.push('/explore/drinks') }
         >
           Explore Drinks
         </button>
