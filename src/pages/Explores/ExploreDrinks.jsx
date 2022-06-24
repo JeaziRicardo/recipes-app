d60b04b030ed18fe9fb0177b8efc0c2c6ed62ddd
NiiVx <vinicios241@gmail.com>
2022-06-24 19:02:55 -0300

    Renderização dos botões nas telas de explore foods e drinks

--- src/pages/Explores/ExploreDrinks.jsx
+++ src/pages/Explores/ExploreDrinks.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import FooterNav from '../../components/FooterNav';
 import Header from '../../components/Header';
+import ExploreButtons from '../../components/ExploreButtons';
 
 function ExploreDrinks() {
   return (
     <div>
       <Header title="Explore Drinks" />
+      <ExploreButtons />
       <FooterNav />
     </div>
   );
--- src/pages/Explores/ExploreFoods.jsx
+++ src/pages/Explores/ExploreFoods.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import FooterNav from '../../components/FooterNav';
 import Header from '../../components/Header';
+import ExploreButtons from '../../components/ExploreButtons';
 
 function ExploreFoods() {
   return (
     <div>
       <Header title="Explore Foods" />
+      <ExploreButtons />
       <FooterNav />
     </div>
   );
