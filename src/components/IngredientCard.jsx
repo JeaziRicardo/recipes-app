4e8c7c448527baa3293e0fd21ac6ada2127ef3b6
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2022-06-28 14:20:34 -0300

    [77] - Muda de rota ao clicar nos cards dos ingredientes

--- src/components/IngredientCard.jsx
+++ src/components/IngredientCard.jsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from 'react';
-import { useHistory } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { fetchByAllIngredient } from '../services/API';
 
 function IngredientCard() {
-  const NUN_LENGTH = 11;
+  const MAX_LENGTH = 11;
   const { location: { pathname } } = useHistory();
   const [ingredients, setIngredients] = useState([]);
 
@@ -26,43 +26,47 @@ function IngredientCard() {
 
       { pathname === '/explore/drinks/ingredients'
         && (
-          ingredients.map(({ strIngredient1 }, index) => index <= NUN_LENGTH && (
+          ingredients.map(({ strIngredient1 }, index) => index <= MAX_LENGTH && (
             <section
               key={ index }
               className="recipeCard"
               data-testid={ `${index}-ingredient-card` }
             >
-              <img
-                data-testid={ `${index}-card-img` }
-                src={ `https://www.thecocktaildb.com/images/ingredients/${strIngredient1}-Small.png` }
-                alt={ `${strIngredient1}` }
-              />
-              <h3
-                data-testid={ `${index}-card-name` }
-              >
-                { `${strIngredient1}` }
-              </h3>
+              <Link to="/drinks">
+                <img
+                  data-testid={ `${index}-card-img` }
+                  src={ `https://www.thecocktaildb.com/images/ingredients/${strIngredient1}-Small.png` }
+                  alt={ `${strIngredient1}` }
+                />
+                <h3
+                  data-testid={ `${index}-card-name` }
+                >
+                  { `${strIngredient1}` }
+                </h3>
+              </Link>
             </section>
           ))) }
 
       { pathname === '/explore/foods/ingredients'
         && (
-          ingredients.map(({ strIngredient }, index) => index <= NUN_LENGTH && (
+          ingredients.map(({ strIngredient }, index) => index <= MAX_LENGTH && (
             <section
               key={ index }
               className="recipeCard"
               data-testid={ `${index}-ingredient-card` }
             >
-              <img
-                data-testid={ `${index}-card-img` }
-                src={ `https://www.themealdb.com/images/ingredients/${strIngredient}-Small.png` }
-                alt={ `${strIngredient}` }
-              />
-              <h3
-                data-testid={ `${index}-card-name` }
-              >
-                { `${strIngredient}` }
-              </h3>
+              <Link to="/foods">
+                <img
+                  data-testid={ `${index}-card-img` }
+                  src={ `https://www.themealdb.com/images/ingredients/${strIngredient}-Small.png` }
+                  alt={ `${strIngredient}` }
+                />
+                <h3
+                  data-testid={ `${index}-card-name` }
+                >
+                  { `${strIngredient}` }
+                </h3>
+              </Link>
             </section>
           ))) }
 
