a6fb8a936f5447219b104d7b3b4098f6d3acab3f
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2022-06-28 14:48:52 -0300

    [79] - Cria o componente DropDown

---
+++ src/components/DropDown.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function DropDown() {
+  return (
+    <section>
+      <select name="" id="">
+        {}
+      </select>
+    </section>
+  );
+}
+
+export default DropDown;
