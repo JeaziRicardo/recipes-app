863e75352b4ff97c453878bcf98c9b460221eb13
Guilherme Axhcar <guilhermeaxhcar@gmail.com>
2022-07-01 22:22:01 -0300

    começo da produção da tela de progresso de receita

--- src/components/RecipeInfo.jsx
+++ src/components/RecipeInfo.jsx
@@ -5,21 +5,42 @@ import { useHistory } from 'react-router-dom';
 // import blackHeartIcon from '../images/blackHeartIcon.svg';
 import whiteHeartIcon from '../images/whiteHeartIcon.svg';
 import shareIcon from '../images/shareIcon.svg';
+import '../Styles/RecipeInfos.css';
 
 function RecipesInfos() {
   const history = useHistory();
   const { location } = history;
+  const [checked, setChecked] = useState([]);
   const [linkCopy, setLinkCopy] = useState(false);
+
   const recipe = useSelector(({ recipes }) => recipes.recipeInfo);
   const recipeArr = Object.entries(recipe);
   const ingredientsArr = recipeArr.filter((recipeKey) => recipeKey[0]
     .includes('strIngredient'));
   const measureArr = recipeArr.filter((recipeKey) => recipeKey[0]
     .includes('strMeasure'));
+
   function copyRecipeLink() {
     navigator.clipboard.writeText(`http://localhost:3000${location.pathname}`);
     setLinkCopy(true);
   }
+
+  function hadleChange(target) {
+    console.log(target.value);
+    if (checked.includes(target.value)) {
+      setChecked(checked.filter((check) => check !== target.value));
+    } else {
+      setChecked([...checked, target.value]);
+    }
+  }
+
+  function updateRecipesInProgress() {
+    const inProgressRecipes = {
+      cocktails: {},
+      meals: {},
+    };
+  }
+
   return (
     <section className="recipe-info">
       <header>
@@ -58,18 +79,45 @@ function RecipesInfos() {
         </h4>
       </header>
       <section className="ingredients-section">
-        <ul>
-          {ingredientsArr.map((ingredient, index) => ingredient[1] !== null
+        {location.pathname.includes('in-progress') ? (
+          <ul>
+            {ingredientsArr.map((ingredient, index) => ingredient[1] !== null
               && ingredient[1].trim() !== ''
             && (
-              <li
+              <label
+                htmlFor={ index }
                 key={ index }
-                data-testid={ `${index}-ingredient-name-and-measure` }
+                data-testid={ `${index}-ingredient-step` }
               >
-                {`${ingredient[1]} - ${measureArr[index][1]}`}
-              </li>
+                <input
+                  type="checkbox"
+                  id={ index }
+                  value={ ingredient[1] }
+                  onClick={ updateRecipesInProgress }
+                  onChange={ ({ target }) => hadleChange(target) }
+                />
+                <li
+                  className={ checked.includes(ingredient[1]) ? 'checked' : 'no-check' }
+                >
+                  {`${ingredient[1]} - ${measureArr[index][1]}`}
+                </li>
+              </label>
             ))}
-        </ul>
+          </ul>
+        ) : (
+          <ul>
+            {ingredientsArr.map((ingredient, index) => ingredient[1] !== null
+                && ingredient[1].trim() !== ''
+              && (
+                <li
+                  key={ index }
+                  data-testid={ `${index}-ingredient-name-and-measure` }
+                >
+                  {`${ingredient[1]} - ${measureArr[index][1]}`}
+                </li>
+              ))}
+          </ul>
+        ) }
       </section>
       <div>
         <p data-testid="instructions">
--- src/pages/Drink/DrinkInProgress.jsx
+++ src/pages/Drink/DrinkInProgress.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import RecipesInfos from '../../components/RecipeInfo';
+import { changeRecipeInfo } from '../../Redux/RecipesReducer';
+import { fetchRecipesInfo } from '../../services/API';
 
 function DrinkInProgress() {
+  const { recipesId } = useParams();
+  const dispatch = useDispatch();
+  async function getDetails() {
+    const response = await fetchRecipesInfo(recipesId, 'drink');
+    const data = response.drinks[0];
+    dispatch(changeRecipeInfo(data));
+  }
+  getDetails();
+
   return (
     <div>
-      <h1>a</h1>
+      <RecipesInfos />
+      <button
+        data-testid="finish-recipe-btn"
+        type="button"
+      >
+        Finish Recipe
+      </button>
     </div>
   );
 }
